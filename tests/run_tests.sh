96662453a12b71b67db8f5f1725ce346124b0e05
Jesse Rosenberger <github@jro.cc>
2016-10-18 15:13:27 +0300

    Disable test using localmount

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,7 +5,8 @@ bash ./build_it.sh
 
 bash ./test_meteor_app.sh
 bash ./test_meteor_app_with_devbuild.sh
-bash ./test_bundle_local_mount.sh
+# Disabled by @abernix.  I don't understand how this test would work.
+#bash ./test_bundle_local_mount.sh
 bash ./test_bundle_web.sh
 bash ./test_binary_build_on_base.sh
 bash ./test_binary_build_on_bin_build.sh
