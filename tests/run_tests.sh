2f9c4442e3daa73139c3f8399fd62598c0a49f39
Jesse Rosenberger <github@jro.cc>
2016-10-18 18:44:14 +0300

    3/8

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,7 +8,7 @@ bash ./test_meteor_app_with_devbuild.sh
 # Disabled by @abernix.  I don't understand how this test would work.
 #bash ./test_bundle_local_mount.sh
 # Disabled by @abernix. I understand how this test works, but it fails and I've chose not to fix it.
-#bash ./test_bundle_web.sh
+bash ./test_bundle_web.sh
 # bash ./test_binary_build_on_base.sh
 # bash ./test_binary_build_on_bin_build.sh
 # bash ./test_phantomjs.sh
